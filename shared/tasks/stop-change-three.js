ff02ba696dabdd27d5a5e64273a33c9a35ecb9f3
Lars Kulbe <112941836+LarsKul@users.noreply.github.com>
2026-09-14 13:53:07 +0200

    update three-button stop change instructions

--- shared/tasks/stop-change-three.js
+++ shared/tasks/stop-change-three.js
@@ -31,6 +31,7 @@
         <p>We will now add a new rule to the task.</p>
         <p>On some trials, the circle around the symbol turns blue shortly after the symbol appears. These are change trials.</p>
         <p>On a change trial, press the key marked with the blue square, no matter which symbol is inside the circle.</p>
+        <p>That is, when the circle is is black, respond as usual. But when the circle turns blue, press the key marked with the blue square with your other hand.</p>
       `,
       signalRule(task, helpers) {
         return `
