081f9bf0ba01eb24263b0b8b7f3896281424c346
Lars Kulbe <112941836+LarsKul@users.noreply.github.com>
2026-09-14 15:12:18 +0200

    update instructions stimulus selective

--- shared/tasks/stimulus-selective.js
+++ shared/tasks/stimulus-selective.js
@@ -34,16 +34,17 @@
       timingDifficulty: "The circle changes color after the symbol appears, so on a stop trial it may sometimes be difficult to stop yourself from responding in time.",
       signalIntro: `
         <p>We will now introduce an extra thing to look out for.</p>
-        <p>On some trials, the circle around the symbol turns red or blue shortly after the symbol appears.</p>
-        <p>Trials where the circle turns red are stop trials. Trials where it turns blue are ignore trials: ignore the blue circle and respond to the symbol on screen.</p>
+        <p>On some trials, the circle around the symbol turns either red or blue shortly after the symbol appears.</p>
+        <p>Trials where the circle turns red are stop trials. On stop trials, try to stop yourself from pressing a key.</p> 
+        <p> Trials where the circle turns blue are ignore trials. On ignore trials, ignore the blue circle and respond to the symbol on screen as usual.</p>
+        <p>That is, when the circle is is black or blue, respond as usual. But when the circle turns red, do not press any key.</p>
       `,
       signalRule(task, helpers) {
         return `
           <p>On a stop trial, when the circle turns red, do not press any response key.</p>
           <p>On an ignore trial, when the circle turns blue, ignore the blue circle and respond to the symbol on screen.</p>
-          ${helpers.renderStimulusActionRows(task, [
-            { orientation: "normal", signal: "red", responseText: "Do not press any key" },
-            { orientation: "rotated", signal: "red", responseText: "Do not press any key" },
+            ${helpers.renderStimulusActionRows(task, [
+            { orientations: ["normal", "rotated"], signal: "red", responseText: "Do not press any key" },
             { orientation: "normal", signal: "blue", key: task.orientationKeys.normal },
             { orientation: "rotated", signal: "blue", key: task.orientationKeys.rotated }
           ])}
